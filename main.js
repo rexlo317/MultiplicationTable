0170d627ce8b3b45678d4cf81e1672918bdb32f9
Rex Lo <LORE2@OOCL.COM>
2018-11-20 18:00:30 +0800

    output table, havnt added other test case

--- main.js
+++ main.js
@@ -19,4 +19,51 @@ function isInputInRange (startNum, endNum){
 	return true;
 }
 
+function generateArray (startNum, endNum){
+	let result = [];
+	let i = startNum;
+	let j = startNum;
+	let count = startNum;
+	while( count <= endNum){
+		while( i <= j ){
+		result.push(i + '*' + j + '=' + i * j );
+		i++;
+		}
+		count++;
+		i = startNum;
+		j++;
+	}
+	return result;
+}
+
+function generateTable (stringArray){
+	let count = 1;
+	let counter = 1;
+	let tempCounter = 0;
+	let innerCount = 0;
+	let result = '';
+	let temp = stringArray.length;
+	while (temp-counter != 0 ){
+		temp = temp - counter;
+		tempCounter = tempCounter + counter;
+		counter ++;
+	}
+	temp = stringArray[temp].length + 1;
+	
+	for (let i=0; i<stringArray.length; i++){
+		innerCount++;
+		if (innerCount>count){
+			result+='\n';
+			innerCount = 1;
+			count++;
+			counter = temp-stringArray[i].length;
+		}
+		result += stringArray[i];
+		if (innerCount < count)
+			for(let j=0;j<counter;j++)
+				result +=' ';
+	}
+	return result;
+}
+
 module.exports = getMultiplicationTable;
